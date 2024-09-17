ca9f565fccb74c64eb2457599772e57f2531ce47
badlzi <155917158+badlzi@users.noreply.github.com>
2024-09-17 17:12:09 +0700

    new_Update

---
+++ src/main/java/UI/index.java
@@ -0,0 +1,19 @@
+package UI;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+@WebServlet(name = "index", value = "/index")
+public class index extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+    }
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
